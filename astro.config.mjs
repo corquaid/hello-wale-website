2e5dbdb67d33757cb4e67732c32b360aa6a9e364
corquaid <69508715+corquaid@users.noreply.github.com>
2026-07-18 19:09:39 +0200

    Changes for Github Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://corquaid.github.io',
+  base: '/hello-wale-website/',
+
   integrations: [react()],
 
   vite: {
